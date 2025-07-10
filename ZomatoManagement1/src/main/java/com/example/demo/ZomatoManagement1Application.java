a2c9eafe9b9b977716fa4affd7d2211127a02c2c
Vaishnavi <chopadevaishnavi85@gmail.com>
2025-07-10 10:38:07 +0530

    new files added

---
+++ ZomatoManagement1/src/main/java/com/example/demo/ZomatoManagement1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZomatoManagement1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZomatoManagement1Application.class, args);
+	}
+
+}
